7f28c79f1120603300afe349b326252946485006
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-02-13 18:47:05 -0300

    chore: create header component

--- arcadePortal/src/App.tsx
+++ arcadePortal/src/App.tsx
@@ -3,9 +3,12 @@ import Header from "./components/Header"
 function App() {
 
   return (
+    <>
+    <Header/>
     <main className='bg-slate-600 h-screen block font-sans'>
-      <Header></Header>
+
     </main>
+    </>
   )
 }
 
