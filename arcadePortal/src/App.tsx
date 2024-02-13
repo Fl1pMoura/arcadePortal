916b9963bdce6e12047108d214a926bc279d39a3
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-02-13 19:01:39 -0300

    Revert "chore: create header component"
    This reverts commit 7f28c79f1120603300afe349b326252946485006.

--- arcadePortal/src/App.tsx
+++ arcadePortal/src/App.tsx
@@ -3,12 +3,9 @@ import Header from "./components/Header"
 function App() {
 
   return (
-    <>
-    <Header/>
     <main className='bg-slate-600 h-screen block font-sans'>
-
+      <Header></Header>
     </main>
-    </>
   )
 }
 
--- arcadePortal/src/components/Header/index.tsx
+++ arcadePortal/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 export default function Header(){
   return(
-  <header className="w-full bg-slate-800 flex justify-center items-center py-4">
-    <h1 className="text-slate-200 text-2xl font-bold">Arcade Portal</h1>
+  <header className="w-full container">
+    <h1 className="text-slate-200 text-2xl font-bold text-center pt-4">Arcade Portal</h1>
   </header>
   )
 }
