eae1023dad5500e332e6935e8e9ef8e9725d9ef9
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-02-13 18:38:37 -0300

    chore: create vite app/ install shadcn ui

---
+++ arcadePortal/src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <main className='bg-slate-600 h-screen block font-sans'>
+      <Header></Header>
+    </main>
+  )
+}
+
+export default App
---
+++ arcadePortal/src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+  return(
+  <header className="w-full container">
+    <h1 className="text-slate-200 text-2xl font-bold text-center pt-4">Arcade Portal</h1>
+  </header>
+  )
+}
