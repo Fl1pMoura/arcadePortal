ccee74172504abfcd73557e18df3ef2f1c9f1eea
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-02-13 19:12:10 -0300

    fix: change directory tree

--- arcadePortal/src/components/Header/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header(){
-  return(
-  <header className="w-full container">
-    <h1 className="text-slate-200 text-2xl font-bold text-center pt-4">Arcade Portal</h1>
-  </header>
-  )
-}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+  return(
+  <header className="w-full bg-slate-800 py-4 flex items-center justify-center">
+    <h1 className="text-slate-200 text-2xl font-bold">Arcade Portal</h1>
+  </header>
+  )
+}
