bbd83e30a3548b7a7c05f08376a70581dc727313
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-02-14 21:19:30 -0300

    chore: add routes

---
+++ src/components/App/index.tsx
@@ -0,0 +1,14 @@
+import Header from "../Header";
+import Router from '../../Routes'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Router/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import GameButton from "@/components/GameButton";
+
+export default function Home(){
+  return(
+    <main className='bg-slate-600 h-screen block font-sans'>
+      <GameButton/>
+    </main>
+  )
+}
